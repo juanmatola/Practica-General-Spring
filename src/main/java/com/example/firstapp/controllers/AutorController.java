7f901e3e2b44bfc8ec41ef3204a92d25f97a134d
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-10-05 19:01:44 -0300

    Creo controlador de Autor

---
+++ src/main/java/com/example/firstapp/controllers/AutorController.java
@@ -0,0 +1,64 @@
+package com.example.firstapp.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.firstapp.entities.Autor;
+import com.example.firstapp.servicios.AutorService;
+
+
+@Controller
+@RequestMapping("/autores")
+public class AutorController {
+	
+	@Autowired
+	private AutorService autorService;
+	
+
+	@GetMapping()
+	public String index(ModelMap modeloDeAutores) {
+		
+		List<Autor> misAutores = autorService.findAll();
+		
+		modeloDeAutores.addAttribute("autores", misAutores);
+		
+		return "autores-list";
+	}
+	
+	@GetMapping("/create")
+	public String create() {
+		//ModelMap model, @PathVariable("insertResult") String inserResult
+		//model.addAttribute("insertResult", inserResult);
+		
+		return "create-autor";
+	}
+	
+	@PostMapping("/create")
+	public String runCreate(ModelMap model, @RequestParam("name") String name) {
+		
+		String res = "redirect:/autores?insertResult=";
+		
+		try {
+			
+			autorService.save(name);
+			
+			res = res.concat("true");
+			
+		} catch (Exception e) {
+			
+			System.err.print("Error al insertar nuevo autor " + e.toString());
+		
+			res = res.concat("false");
+			
+		}
+
+		return res;
+	}
+}
