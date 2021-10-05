30045b9b339a7d11644afa58c42122fee5a6c8b8
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-10-05 19:04:12 -0300

    AutorService refactor

--- src/main/java/com/example/firstapp/servicios/AutorService.java
+++ src/main/java/com/example/firstapp/servicios/AutorService.java
@@ -1,10 +1,12 @@
 package com.example.firstapp.servicios;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.firstapp.entities.Autor;
 import com.example.firstapp.repositories.AutorRepository;
 
@@ -15,8 +17,32 @@ public class AutorService {
 	@Autowired
 	private AutorRepository repoAutor;
 	
-
+	@Transactional(propagation = Propagation.REQUIRED, rollbackFor = { Exception.class })
+	public Autor save(String name) throws Exception {
+		
+		return repoAutor.save(this.createAutor(name));
+		
+	}
+	
+	public Autor createAutor(String name) {
+		return new Autor(name);
+	}
+	
+	public Autor findById(String id) throws Exception {
+		
+		Optional<Autor> res = repoAutor.findById(id);
+		
+		if(!res.isEmpty()) {
+			return res.get();
+		}else {
+			throw new Exception("No existe autor con dicho id");
+		}
+		
+	}
+	
 	public List<Autor> findAll(){
+		
 		return repoAutor.findAll();
+
 	}
 }
