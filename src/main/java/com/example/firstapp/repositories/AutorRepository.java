a0632fbba00bd902cfec037124c465ad9aca844e
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-10-05 19:03:54 -0300

    Creo metodo findById en AutorRepository

--- src/main/java/com/example/firstapp/repositories/AutorRepository.java
+++ src/main/java/com/example/firstapp/repositories/AutorRepository.java
@@ -1,10 +1,19 @@
 package com.example.firstapp.repositories;
 
 import com.example.firstapp.entities.Autor;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, String> {
+	
+	
+	@Query("SELECT a FROM Autor a WHERE a.id = :id")
+	public Optional<Autor> findById(@Param("id") String id);
 	
 }
