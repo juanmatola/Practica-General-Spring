46b244615c1c733130830e16f7854f310fafd5c0
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-10-13 16:06:12 -0300

    Reestructuro directorio de vistas

--- src/main/java/com/example/firstapp/controllers/AutorController.java
+++ src/main/java/com/example/firstapp/controllers/AutorController.java
@@ -15,123 +15,123 @@ import com.example.firstapp.entities.Autor;
 import com.example.firstapp.interfaces.ErrorHandler;
 import com.example.firstapp.services.AutorService;
 
-
 @Controller
 @RequestMapping("/autores")
-public class AutorController implements ErrorHandler  {
-	
+public class AutorController implements ErrorHandler {
+
 	@Autowired
 	private AutorService autorService;
-	
+	private final String viewPath = "autores/";
+
 	@GetMapping()
 	public String index(ModelMap modeloDeAutores) {
-		
+
 		List<Autor> misAutores = autorService.findAll();
-		
+
 		modeloDeAutores.addAttribute("autores", misAutores);
-		
-		return "autores-list";
+
+		return this.viewPath.concat("autores-list");
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
-		
-		return "create-autor";
-		
+
+		return this.viewPath.concat("create-autor");
+
 	}
-	
+
 	@PostMapping("/create")
 	public String runCreate(ModelMap model, @RequestParam("name") String name) {
-		
+
 		try {
-			
+
 			autorService.save(name);
-			
+
 			return "redirect:/autores";
-			
+
 		} catch (Exception e) {
-		
+
 			return this.errorHandle(e, model);
-			
+
 		}
 
 	}
-	
+
 	@GetMapping("/update/alta/{id}")
 	public String updateAlta(ModelMap model, @PathVariable("id") String id) {
-		
-		try {			
-			
+
+		try {
+
 			autorService.chageAltaById(id);
-			
+
 			return "redirect:/autores";
-			
+
 		} catch (Exception e) {
-			
+
 			return this.errorHandle(e, model);
-			
+
 		}
-		
+
 	}
-	
+
 	@GetMapping("/update/name/{id}")
 	public String updateName(ModelMap model, @PathVariable("id") String id) {
-		
+
 		try {
-			
+
 			Autor autor = autorService.findById(id);
 			model.addAttribute("autor", autor);
-			
-			return "update-autor";
-			
+
+			return this.viewPath.concat("update-autor");
+
 		} catch (Exception e) {
-			
+
 			return this.errorHandle(e, model);
-			
+
 		}
-		
+
 	}
-	
+
 	@PostMapping("/update/name/{id}")
 	public String changeName(ModelMap model, @PathVariable("id") String id, @RequestParam("name") String name) {
-		
+
 		try {
-			
+
 			autorService.update(id, name);
-			
+
 			return "redirect:/autores";
-			
+
 		} catch (Exception e) {
-			
+
 			return this.errorHandle(e, model);
-			
+
 		}
-		
+
 	}
-	
+
 	@GetMapping("/remove/{id}")
 	public String remove(ModelMap model, @PathVariable("id") String id) {
-		
+
 		try {
-			
+
 			autorService.removeById(id);
-			
+
 			return "redirect:/autores";
-			
+
 		} catch (Exception e) {
-			
+
 			return this.errorHandle(e, model);
-			
+
 		}
-		
+
 	}
-	
+
 	@Override
 	public String errorHandle(Exception e, ModelMap model) {
-		
+
 		model.addAttribute("err", e.getMessage());
-		
+
 		return this.index(model);
 	}
-	
+
 }
